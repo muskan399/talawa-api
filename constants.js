1390c1c30226fbf0622fc9da6ac31c0fe2f7aed4
Muskan Modi <63671368+muskan399@users.noreply.github.com>
2021-09-30 13:54:47 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 let URL = 'http://calico.palisadoes.org/talawa/graphql';
 
 if (process.env.NODE_ENV === 'test') {
-  URL = 'http://talawa-graphql-api.herokuapp.com/graphql';
+  URL = 'http://localhost:4000/graphql';
 }
 
 module.exports.URL = URL;
